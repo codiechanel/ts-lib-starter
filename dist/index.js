c8d1d2db2a77f0cf9e9ca73ab581191471565dd8
codiechanel <codiechanel@gmail.com>
2018-08-25 15:45:37 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hello_1 = require("./hello");
+exports.sayHello = hello_1.sayHello;
+exports.sayGoodbye = hello_1.sayGoodbye;
+//# sourceMappingURL=index.js.map
